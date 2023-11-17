202c749e58130b2255c46da15dc1a7821f693058
PieterJanDelie <pieterjan.delie@gmail.com>
2023-11-17 10:11:19 +0100

    first init

---
+++ src/main/java/be/vives/ti/imageCalender/ImageCalenderApplication.java
@@ -0,0 +1,13 @@
+package be.vives.ti.imageCalender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageCalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageCalenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/vives/ti/imageCalender/ImageCalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vives.ti.imageCalender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageCalenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
